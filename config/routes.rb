43e417f01155f1288ab004c78fe382aee2db505c
17Points <vlad.sotoqz@gmail.com>
2016-04-02 02:53:05 +0000

    Simple Hello World

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  #Test to Index Page 
+  root 'pages#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
